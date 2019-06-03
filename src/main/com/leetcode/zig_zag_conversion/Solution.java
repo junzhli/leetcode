3ce135e95c10da396255ef7550b619f405ae70e4
Jeremy Li <junzhli@gmail.com>
2019-06-03 09:31:07 +0800

    Added better solution with big o n

--- src/main/com/leetcode/zig_zag_conversion/Solution.java
+++ src/main/com/leetcode/zig_zag_conversion/Solution.java
@@ -82,3 +82,45 @@ public class Solution {
         }
     }
 }
+
+/**
+ * class Solution {
+ *     public String convert(String s, int numRows) {
+ *
+ *         if (numRows == 1) return s;
+ *
+ *         List<StringBuilder> rows = new ArrayList<>();
+ *         for (int i = 0; i < Math.min(numRows, s.length()); i++)
+ *             rows.add(new StringBuilder());
+ *
+ *         int curRow = 0;
+ *         boolean goingDown = false;
+ *
+ *         for (char c : s.toCharArray()) {
+ *             rows.get(curRow).append(c);
+ *             if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+ *             curRow += goingDown ? 1 : -1;
+ *         }
+ *
+ *         StringBuilder ret = new StringBuilder();
+ *         for (StringBuilder row : rows) ret.append(row);
+ *         return ret.toString();
+ *     }
+ * }
+ *
+ * Complexity Analysis
+ *
+ *     Time Complexity: O(n)O(n)O(n), where n==len(s)n == \text{len}(s)n==len(s)
+ *     Space Complexity: O(n)O(n)O(n)
+ *
+ *
+ * (row0) : stringBuilder + .append(char) => goDown = !goDown
+ * (row1) : stringBuilder + .append(char) => goDown
+ * (row2) : stringBuilder + .append(char) => goDown
+ * (row3) : stringBuilder + .append(char) => goDown
+ * .
+ * .
+ * .
+ * .
+ * (rowN) : stringBuilder + .append(char) => goDown = !goDown
+ */
